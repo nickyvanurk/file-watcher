932dcfb4e2e7052607f314b04243cd421e67f281
nickyvanurk <nickyvanurk@gmx.com>
2016-11-08 23:05:15 +0100

    Added command-line arguments support

--- file_watcher.py
+++ file_watcher.py
@@ -1,5 +1,20 @@
+import collections, argparse
+
+def get_command_line_args():
+  defaults = {'path': '.', 'filename': None,'extensions': '*', 'commands': 'make'}
+
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-p', dest='path', help='path of the directory tree to watch')
+  parser.add_argument('-f', dest='filename', help='path of the file to watch')
+  parser.add_argument('-e', dest='extensions', help='extentions to watch')
+  parser.add_argument('-c', dest='commands', help='commands to execute')
+  namespace = parser.parse_args()
+  command_line_args = {k:v for k, v in vars(namespace).items() if v}
+
+  return collections.ChainMap(command_line_args, defaults)
+
 def main():
-  pass
+  args = get_command_line_args()
 
 if __name__ == '__main__':
   main()
--- test_file_watcher.py
+++ test_file_watcher.py
@@ -1,4 +1,12 @@
 import file_watcher
 
+def test_get_command_line_args():
+  args = file_watcher.get_command_line_args()
+
+  assert args['path'] == '.'
+  assert args['filename'] == None
+  assert args['extensions'] == '*'
+  assert args['commands'] == 'make'
+
 def test_main():
   file_watcher.main()
