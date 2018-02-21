c2e7663774e0ce3e4e8b23d49927155d78ea000a
Nicky van Urk <nickyvanurk@gmx.com>
2018-02-21 15:33:29 +0100

    Remove unused test

--- file_watcher/tests/test_app.py
+++ file_watcher/tests/test_app.py
@@ -1,6 +1,7 @@
 import os, file_watcher
 from testfixtures import TempDirectory
 
+
 tempdir = TempDirectory()
 path = tempdir.makedir('tests/fixtures')
 tempdir.write('tests/fixtures/test.txt', b'')
@@ -8,21 +9,17 @@ tempdir.write('tests/fixtures/test.txt', b'')
 def teardown_module(module):
   TempDirectory.cleanup_all();
 
-# def test_get_command_line_args():
-#   args = file_watcher.get_command_line_args()
-#   assert args['path'] == '.'
-#   assert args['filename'] == None
-#   assert args['extensions'] == '*'
-#   assert args['commands'] == 'make'
 
 def test_get_files_in_dir():
   files = file_watcher.get_files_in_dir(path)
   assert len(files) == 1
   assert files[0] == path + '/test.txt'
 
+
 def test_get_file_last_mod():
   last_mod_time = file_watcher.get_file_last_mod(path + '/test.txt')
   assert last_mod_time == os.stat(path + '/test.txt')[8]
+
 
 def test_get_files_last_mod():
   files = file_watcher.get_files_in_dir(path)
@@ -30,10 +27,9 @@ def test_get_files_last_mod():
   assert len(files_last_mod) == 1
   assert files_last_mod[0] == os.stat(files[0])[8]
 
+
 def test_get_files_data():
   files_data = file_watcher.get_files_data(path)
   assert len(files_data) == 1
   assert list(files_data.keys())[0] == path + '/test.txt'
   assert list(files_data.values())[0] == os.stat(path + '/test.txt')[8]
-
-
