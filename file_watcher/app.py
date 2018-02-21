91e51b76c173332a95003c77ac8d68a9cd2afd4d
Nicky van Urk <nickyvanurk@gmx.com>
2018-02-21 15:38:52 +0100

    Improve code style

--- file_watcher/app.py
+++ file_watcher/app.py
@@ -2,65 +2,65 @@
 import contextlib
 import time
 import os
-import collections
-import argparse
 import click
 
 
 @click.command()
-@click.option('--path', '-p',  default='.', help='Path of directory tree to watch.')
+@click.option('--path', '-p',  default='.',
+              help='Path of directory tree to watch.')
 @click.option('--file', '-f', default=None, help='Path of file to watch.')
 @click.option('--exts', '-e', default='*', help='Extensions to watch.')
 @click.option('--cmds', '-c', default='make', help='Commands to execute.')
-@click.option('--interval', '-i', default=500, help='Scan interval in milliseconds.')
+@click.option('--interval', '-i', default=500,
+              help='Scan interval in milliseconds.')
 def main(path, file, exts, cmds, interval):
-  files_data = get_files_data(path)
-
-  while True:
-    time.sleep(int(interval) / 1000)
-
-    temp = get_files_data(path)
-    unshared_items = set(files_data.items()) ^ set(temp.items())
-
-    if len(unshared_items) != 0:
-      filename = unshared_items.pop()[0]
-      file_ext = filename.split('.')[-1]
-
-      if filename == file or '*' == exts or file_ext in exts:
-        os.system(cmds)
-
     files_data = get_files_data(path)
+
+    while True:
+        time.sleep(int(interval) / 1000)
+
+        temp = get_files_data(path)
+        unshared_items = set(files_data.items()) ^ set(temp.items())
+
+        if len(unshared_items) != 0:
+            filename = unshared_items.pop()[0]
+            file_ext = filename.split('.')[-1]
+
+            if filename == file or '*' == exts or file_ext in exts:
+                os.system(cmds)
+
+        files_data = get_files_data(path)
 
 
 def get_files_in_dir(path):
-  file_list = []
-  for dir_name, subdirs, files in os.walk(path):
-    for file in files:
-      file_list.append(os.path.join(dir_name, file))
+    file_list = []
+    for dir_name, subdirs, files in os.walk(path):
+        for file in files:
+            file_list.append(os.path.join(dir_name, file))
 
-  return file_list
+    return file_list
 
 
 def get_file_last_mod(path):
-  return os.stat(path)[8];
+    return os.stat(path)[8]
 
 
 def get_files_last_mod(files):
-  files_last_mod = []
-  for file_path in files:
-    file_last_mod = get_file_last_mod(file_path)
-    files_last_mod.append(file_last_mod)
+    files_last_mod = []
+    for file_path in files:
+        file_last_mod = get_file_last_mod(file_path)
+        files_last_mod.append(file_last_mod)
 
-  return files_last_mod
+    return files_last_mod
 
 
 def get_files_data(path):
-  files = get_files_in_dir(path)
-  files_last_mod = get_files_last_mod(files)
+    files = get_files_in_dir(path)
+    files_last_mod = get_files_last_mod(files)
 
-  return dict(zip(files, files_last_mod))
+    return dict(zip(files, files_last_mod))
 
 
 if __name__ == '__main__':
-  with contextlib.suppress(KeyboardInterrupt):
-    main()
+    with contextlib.suppress(KeyboardInterrupt):
+        main()
--- file_watcher/tests/test_app.py
+++ file_watcher/tests/test_app.py
@@ -1,4 +1,5 @@
-import os, file_watcher
+import os
+import file_watcher
 from testfixtures import TempDirectory
 
 
@@ -6,30 +7,31 @@ tempdir = TempDirectory()
 path = tempdir.makedir('tests/fixtures')
 tempdir.write('tests/fixtures/test.txt', b'')
 
+
 def teardown_module(module):
-  TempDirectory.cleanup_all();
+    TempDirectory.cleanup_all()
 
 
 def test_get_files_in_dir():
-  files = file_watcher.get_files_in_dir(path)
-  assert len(files) == 1
-  assert files[0] == path + '/test.txt'
+    files = file_watcher.get_files_in_dir(path)
+    assert len(files) == 1
+    assert files[0] == path + '/test.txt'
 
 
 def test_get_file_last_mod():
-  last_mod_time = file_watcher.get_file_last_mod(path + '/test.txt')
-  assert last_mod_time == os.stat(path + '/test.txt')[8]
+    last_mod_time = file_watcher.get_file_last_mod(path + '/test.txt')
+    assert last_mod_time == os.stat(path + '/test.txt')[8]
 
 
 def test_get_files_last_mod():
-  files = file_watcher.get_files_in_dir(path)
-  files_last_mod = file_watcher.get_files_last_mod(files)
-  assert len(files_last_mod) == 1
-  assert files_last_mod[0] == os.stat(files[0])[8]
+    files = file_watcher.get_files_in_dir(path)
+    files_last_mod = file_watcher.get_files_last_mod(files)
+    assert len(files_last_mod) == 1
+    assert files_last_mod[0] == os.stat(files[0])[8]
 
 
 def test_get_files_data():
-  files_data = file_watcher.get_files_data(path)
-  assert len(files_data) == 1
-  assert list(files_data.keys())[0] == path + '/test.txt'
-  assert list(files_data.values())[0] == os.stat(path + '/test.txt')[8]
+    files_data = file_watcher.get_files_data(path)
+    assert len(files_data) == 1
+    assert list(files_data.keys())[0] == path + '/test.txt'
+    assert list(files_data.values())[0] == os.stat(path + '/test.txt')[8]
